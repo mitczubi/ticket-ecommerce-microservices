81d6b178209a531bdf3553d0822ea9654cb9c97a
Mitchell <mitchellzubich@gmail.com>
2023-09-21 08:59:02 -0400

    Adding sign in functionality. Add validateRequest middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response}  from "express";
-import { body, validationResult } from "express-validator";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken"
 
+import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/User";
-import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
@@ -18,13 +18,8 @@ router.post('/api/users/signup',
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-
         const { email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
@@ -52,6 +47,7 @@ router.post('/api/users/signup',
         }
 
         res.status(201).send(user)
-});
+    }
+);
 
 export { router as signupRouter };
